9053a69d35e83a2030363d952c6cab97064d8df0
Tyler Uhl <69764300+tyleruhl02@users.noreply.github.com>
2025-05-06 22:26:49 -0400

    Update Hero.tsx
    Changed main Hero text to "Turning Ideas into Reality"

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
 
           <TextGenerateEffect 
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Turning Ideas into Reality"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
